62bf8accd9132895eb959f8674bb1c88b88f1449
Guilherme Peinador Gomes <guipeinador@hotmail.com>
2023-04-19 16:28:04 -0300

    Código de comunicação

--- comunicacao.py
+++ comunicacao.py
@@ -1,60 +1,19 @@
-import ephem
-import math
-import pandas as pd
-import teste
+import numpy as np
+
+VetorTerraEstacao =
+PEstacao =
+VetorEstacao =
+VetorSatelite =
+VetorSateliteEstacao = VetorSatelite - VetorEstacao
 
 
-'''
-# Define a localização do observador
-observer = ephem.Observer()
-observer.lat = '-5.836126' # Latitude da antena do INPE - Natal
-observer.lon = '-35.207609' # Longitude da antena do INPE - Natal
-observer.elevation = '56'   # Altitude da antena do INPE - Natal (METROS)
-'''
-#df = pd.read_csv(teste.resource_path("data/dados_ECI.csv"), sep='=', engine='python', on_bad_lines='skip')  #********* n calcula lat e long?
-'''   
-latitude = df['7']
-longitude = df['8']
-
-for i in range( 0 , len(latitude) , 1 ): # inicio, fim, passo
-
-    # Define as coordenadas do satélite em função do tempo
-    #renan
-    #satellite_lat = '' # Ler a partir do arquivo gerado na simulação
-    #satellite_lon = '' # Ler a partir do arquivo gerado na simulação
-
-    # Cria um objeto "PyEphem" para o satélite
-    satellite = ephem.FixedBody()
-    satellite._ra = ephem.degrees(longitude[i])
-    satellite._dec = ephem.degrees(latitude[i])
-
-    # Calcula a posição do satélite em relação ao observador
-    satellite.compute(observer)
-
-    elevation = []
-
-    # Calcula a elevação do satélite em relação ao horizonte do observador
-    elevation = math.degrees(satellite.alt)
-
-# Exibe a elevação calculada
-print("A elevação do satélite é:", elevation, "graus")
+#Critério de Comunicação
+gamma = np.pi \
+        - np.arccos((np.dot(VetorSatelite,VetorSateliteEstacao))/np.linalg.norm(VetorSatelite)*np.linalg.norm(VetorSateliteEstacao)) \
+        - np.arccos((np.dot(VetorEstacao,VetorSatelite))/np.linalg.norm(VetorEstacao)*np.linalg.norm(VetorSatelite))
 
 
-
-elevationTF = []
-
-for i in elevation:
-    if elevation[i]>=15:
-        elevationTF.append(True)
-    else:
-        elevationTF.append(False)
-
-elevationTF.count(True)
-'''
-
-import pickle
-
-long = pickle.load(open(('long.pickle'), 'rb'))
-lat = pickle.load(open(('lat.pickle'), 'rb'))
-data = pickle.load(open(('data.pickle'), 'rb'))
-
+if gamma >= 105: #90 graus (horizonte) + 15 (elevação)
+    print("1")
+else:
+    print("0")
